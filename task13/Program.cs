40c8f90fd04f0ae1b34bc1b29ef95ffd99b44f40
Aleksei <spacerabbit1@mail.ru>
2022-09-20 13:05:39 +0700

    task13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num < 100)
+{
+    Console.WriteLine("Третьей цифры нет.");
+}
+if(num >= 100)
+{
+    while(num > 1000)
+    {
+        num = num / 10;
+    }
+    if(num < 1000)
+    {
+        num = num % 10;
+    }
+    Console.WriteLine(num);    
+}
