c77ad681ccab7e56a295444f74f8d3b565a87c82
Aleksei <spacerabbit1@mail.ru>
2022-09-20 12:10:37 +0700

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+while(num > 100)
+{
+    num = num / 10;
+}
+if (num < 100)
+{
+    num = num % 10;
+}
+Console.WriteLine(num);
